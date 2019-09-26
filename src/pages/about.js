5aaeb3a602578bd02e749597ee64c7769cce66ce
ShinKano <52626222+ShinKano@users.noreply.github.com>
2019-09-26 18:45:02 +0900

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+//
+import Title from '../components/title/Title';
+import AboutSection from '../components/about/AboutSection';
+import FeaturesSection from '../components/features/FeaturesSection';
+import FourColumn from '../components/fourcolumn/FourColumn';
+
+export default () => (
+  <SampleSite>
+
+  </SampleSite>
+);
+
+const SampleSite = styled.div`
+  font-family: 'Muli', sans-serif;
+  margin: -2rem;
+`;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+//
+
+
+export default () => (
+  <SampleSite>
+
+  </SampleSite>
+);
+
+const SampleSite = styled.div`
+  font-family: 'Muli', sans-serif;
+  margin: -2rem;
+`;
